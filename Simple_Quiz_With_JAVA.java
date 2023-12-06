b8a14fa9f1d83742eb45f4fdb8fae23578ce86ad
Codensator <86786647+Codensator@users.noreply.github.com>
2023-12-06 11:21:21 +0100

    Made it shuffle the questions and comments/strings english

--- Simple_Quiz_With_JAVA.java
+++ Simple_Quiz_With_JAVA.java
@@ -1,21 +1,31 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
+//This code should provide a simple way to study with single-choice-questions
+//Python would have probably worked better, but I wanted a quick-fix for my exam prep without teaching myself a new language
+//you simply enter the letter for your answer in the console and hit enter
+//after you're done with all the questions you get the percentage of right answers and the questions and right answers to all your wrong answers
 
 public class Simple_Quiz_With_JAVA
 {
     public static void main(String[] args) {
-        // Erstellen der Map mit Fragen und Antworten
+        // Creates a map wehre the key is the questions and the value is the right answer
         Map<String, String> questions = new HashMap<>();
-        questions.put("Frage 1: Was ist die Hauptstadt von Deutschland? A) Berlin B) München C) Hamburg D) Köln", "A");
-        questions.put("Frage 2: Wie viele Kontinente gibt es? A) 5 B) 6 C) 7 D) 8", "C");
-        // Weitere Fragen können hier hinzugefügt werden
+        questions.put("Question 1: What is the capital of Germany? A) Berlin B) Munich C) Hamburg D) Cologne", "A");
+        questions.put("Question 2: What is the capital of England? A) London B) Manchester C) Liverpool D) Bristol", "A");
+        // you could add new questions here
 
+
+        // turns the map into a List
+        List<Map.Entry<String, String>> questionList = new ArrayList<>(questions.entrySet());
+        // shuffles the questions in the list
+        Collections.shuffle(questionList);
+
+        //This method checks your input
         Scanner scanner = new Scanner(System.in);
         int correctAnswers = 0;
         Map<String, String> wrongAnswers = new HashMap<>();
 
-        for (Map.Entry<String, String> entry : questions.entrySet()) {
+        for (Map.Entry<String, String> entry : questionList) {
             System.out.println(entry.getKey());
             String userAnswer = scanner.nextLine().toUpperCase();
 
@@ -26,18 +36,19 @@ public class Simple_Quiz_With_JAVA
             }
         }
 
-        // Prozentuale Anzahl der richtigen Antworten ausgeben
+        // percentage of right answers
         double percentageCorrect = 100.0 * correctAnswers / questions.size();
-        System.out.println("Richtige Antworten: " + percentageCorrect + "%");
+        System.out.println("***** QUIZ-FINNISHED ***** \n Correct Answers: " + percentageCorrect + "%");
 
-        // Falsch beantwortete Fragen anzeigen
+
+        // shows the wrong answered questions with the right answer
         if (!wrongAnswers.isEmpty()) {
-            System.out.println("Falsch beantwortete Fragen:");
+            System.out.println("\n" + "Incorrectly answered questions:");
             for (Map.Entry<String, String> entry : wrongAnswers.entrySet()) {
-                System.out.println(entry.getKey() + " Richtig wäre: " + entry.getValue());
+                System.out.println(entry.getKey() + " Correct would be: " + entry.getValue());
             }
         } else {
-            System.out.println("Alle Fragen richtig beantwortet!");
+            System.out.println("All questions answered correctly!");
         }
     }
 }
